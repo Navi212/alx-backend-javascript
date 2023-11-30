2268598ce114909c90c399b0ec75d0c817821d5b
Joseph Nweke <josephchristo615@gmail.com>
2023-11-30 22:49:36 +0100

    Removes comment

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,13 +1,10 @@
 export default function taskBlock(trueOrFalse) {
-  const task = false;
-  const task2 = true;
+  var task = false;
+  var task2 = true;
 
   if (trueOrFalse) {
-    // eslint-disable-next-line
-    const task = true;
-    // eslint-disable-next-line
-    const task2 = false;
+    let task = true;
+    let task2 = false;
   }
-
   return [task, task2];
 }
