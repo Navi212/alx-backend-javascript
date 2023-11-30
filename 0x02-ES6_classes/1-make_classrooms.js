98663459629cc082eb5c3eb66cb9865a2d40ad12
Joseph Nweke <josephchristo615@gmail.com>
2023-11-30 23:29:53 +0100

    Removes camelcasing

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -1,9 +1,9 @@
 import ClassRoom from './0-classroom';
 
 export default function initializeRooms() {
-  const cls_obj1 = new ClassRoom(19);
-  const cls_obj2 = new ClassRoom(20);
-  const cls_obj3 = new ClassRoom(34);
+  const clsObj1 = new ClassRoom(19);
+  const clsObj2 = new ClassRoom(20);
+  const clsObj3 = new ClassRoom(34);
 
-  return [cls_obj1, cls_obj2, cls_obj3];
+  return [clsObj1, clsObj2, clsObj3];
 }
