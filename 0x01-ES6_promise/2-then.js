6848fbe47c6a5af3d5a11d58ba2cecd000b20051
Joseph Nweke <josephchristo615@gmail.com>
2023-12-01 22:00:27 +0100

    Adds task 2 file

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,14 +1,11 @@
 export default function handleResponseFromAPI(promise) {
-  return new Promise((resolve, reject) => {
-    if (promise) {
-      const obj = {
-        status: 200,
-        body: 'success',
-      };
-      console.log('Got a response from the API');
-      resolve(obj);
-    } else {
-      reject(Error);
-    }
-  });
+  return promise.then(() => {
+    const obj = {
+      status: 200,
+      body: 'success',
+    };
+    return obj;
+  })
+    .catch(() => { Error(); })
+    .finally(console.log('Got a response from the API'));
 }
