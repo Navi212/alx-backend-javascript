e8c7556933c8c46e8885885857f08fef2970a571
Joseph Nweke <josephchristo615@gmail.com>
2023-12-01 18:30:54 +0100

    Adds task1 file

---
+++ 0x01-ES6_promise/1-promise.js
@@ -0,0 +1,14 @@
+export default function getFullResponseFromAPI(success) {
+  const promise = new Promise((resolve, reject) => {
+    if (success) {
+      const obj = {
+        status: 200,
+        body: 'Success',
+      };
+      resolve(obj);
+    } else {
+      reject(Error('The fake API is not working currently'));
+    }
+  });
+  return promise;
+}
