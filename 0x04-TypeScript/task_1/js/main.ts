31540cce6961a1ff46bfbceddbbeaf23388e7f36
Joseph Nweke <josephchristo615@gmail.com>
2023-12-08 11:12:14 +0100

    Adds task1 TypeScript file

---
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -0,0 +1,28 @@
+// Let's build a Teacher interface with below characteristics
+/* firstName(string) and lastName(string). These two attributes 
+should only be modifiable when a Teacher is first initialized
+fullTimeEmployee(boolean) this attribute should always be defined
+yearsOfExperience(number) this attribute is optional
+location(string) this attribute should always be defined
+Add the possibility to add any attribute to the Object like contract(boolean)
+without specifying the name of the attribute
+*/
+
+interface Teacher {
+    readonly firstName: string;
+    readonly lastName: string;
+    fullTimeEmployee: boolean;
+    yearsOfExperience?: number;
+    location: string;
+    [key: string]: any;
+}
+
+const teacher3: Teacher = {
+    firstName: 'John',
+    fullTimeEmployee: false,
+    lastName: 'Doe',
+    location: 'London',
+    contract: false,
+  };
+  
+  console.log(teacher3);
