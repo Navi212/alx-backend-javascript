5e1e301230af7aed1f3e35116db86fdaf16c69bc
Joseph Nweke <josephchristo615@gmail.com>
2023-12-08 11:10:42 +0100

    Adds js generated files

---
+++ 0x04-TypeScript/task_1/dist/js/main.js
@@ -0,0 +1,16 @@
+// Let's build a Teacher interface with below characteristics
+/* firstName(string) and lastName(string). These two attributes should only be modifiable when a Teacher is first initialized
+fullTimeEmployee(boolean) this attribute should always be defined
+yearsOfExperience(number) this attribute is optional
+location(string) this attribute should always be defined
+Add the possibility to add any attribute to the Object like contract(boolean) without specifying the name of the attribute
+*/
+var teacher3 = {
+    firstName: 'John',
+    fullTimeEmployee: false,
+    lastName: 'Doe',
+    location: 'London',
+    contract: false,
+};
+console.log(teacher3);
+//# sourceMappingURL=main.js.map
