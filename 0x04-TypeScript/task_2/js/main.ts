fadf691da8bff1542096a55a0cf06775af4f8b1c
Joseph Nweke <josephchristo615@gmail.com>
2023-12-08 14:20:53 +0100

    Adds task_2

---
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -0,0 +1,67 @@
+// Advanced types Part 1
+
+// Creates the DirectorInterface interface with the 3 expected methods:
+interface DirectorInterface {
+    workFromHome(): string;
+    getCoffeeBreak(): string;
+    workDirectorTasks(): string;
+}
+
+// Creates the TeacherInterface interface with the 3 expected methods:
+interface TeacherInterface {
+    workFromHome(): string;
+    getCoffeeBreak(): string;
+    workTeacherTasks(): string;
+}
+
+// Creates a class Director that will implement DirectorInterface
+class Director implements DirectorInterface {
+    workFromHome() {
+        return "Working from home";
+    }
+
+    getCoffeeBreak() {
+        return "Getting a coffee break";
+    }
+
+    workDirectorTasks() {
+        return "Getting to director tasks";
+    }
+}
+
+
+// Create a class Teacher that will implement TeacherInterface
+class Teacher implements TeacherInterface {
+    workFromHome() {
+        return "Cannot work from home";
+    }
+
+    getCoffeeBreak() {
+        return "Cannot have a break";
+    }
+
+    workTeacherTasks() {
+        return "Getting to work";
+    }
+}
+
+/*
+Create a function createEmployee with the following requirements:
+
+It can return either a Director or a Teacher instance
+It accepts 1 arguments:
+salary(either number or string)
+if salary is a number and less than 500 - It should return
+a new Teacher. Otherwise it should return a Director
+*/
+function createEmployee(salary: number | string): Teacher | Director {
+    if (typeof salary === "number" && salary < 500) {
+        return new Teacher();
+    } else {
+        return new Director();
+    }
+};
+
+console.log(createEmployee(200));
+console.log(createEmployee(1000));
+console.log(createEmployee('$500'));
