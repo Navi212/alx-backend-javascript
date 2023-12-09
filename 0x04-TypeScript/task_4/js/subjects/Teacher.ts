45185412704e23ef54855ea9023ffbc323dfc184
Joseph Nweke <josephchristo615@gmail.com>
2023-12-09 11:31:54 +0100

    Adds comment to subject files

--- 0x04-TypeScript/task_4/js/subjects/Teacher.ts
+++ 0x04-TypeScript/task_4/js/subjects/Teacher.ts
@@ -1,3 +1,8 @@
+// Task 9
+// Create a file Teacher.ts and write
+// a Teacher interface in a namespace named Subjects.
+//    interface requires firstName and lastName as string
+
 namespace Subjects {
     export interface Teacher {
         firstName: string;
