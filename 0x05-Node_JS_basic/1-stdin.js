44467b3ea45cdb7dd6fff74677c096366f6d2d64
Joseph Nweke <josephchristo615@gmail.com>
2024-04-29 23:24:58 +0100

    Fix: Replaced let with const

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -8,14 +8,19 @@ The program should display Your name is: INPUT
 When the user ends the program, it should display
 `This important software is now closing` (followed by a new line)
  */
-const text = "Welcome to Holberton School, what is your name?\n"
-process.stdout.write(`${text}`)
-process.stdin.on("readable", ()=>{
-    const chunk = process.stdin.read();
-    if (chunk !== null){
-        process.stdout.write(`Your name is: ${chunk}`);
-    }
-});
-process.stdin.on("end", ()=>{
-    process.stdout.write("This important software is now closing\n");
-});
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
+
+if (process.stdin.isTTY) {
+  process.stdin.on('data', (data) => {
+    process.stdout.write(`Your name is: ${data.toString()}`);
+    process.exit();
+  });
+} else {
+  process.stdin.on('data', (data) => {
+    process.stdout.write(`Your name is: ${data.toString()}`);
+    process.exit();
+  });
+  process.on('exit', () => {
+    process.stdout.write('This important software is now closing\n');
+  });
+}
