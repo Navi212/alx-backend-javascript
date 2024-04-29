6a67532fda5a0b5a497190a5c5c5cae6e38f2514
Joseph Nweke <josephchristo615@gmail.com>
2024-04-29 23:28:00 +0100

    Fix: Rolled back to first code

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -8,19 +8,14 @@ The program should display Your name is: INPUT
 When the user ends the program, it should display
 `This important software is now closing` (followed by a new line)
  */
-process.stdout.write('Welcome to Holberton School, what is your name?\n');
-
-if (process.stdin.isTTY) {
-  process.stdin.on('data', (data) => {
-    process.stdout.write(`Your name is: ${data.toString()}`);
-    process.exit();
-  });
-} else {
-  process.stdin.on('data', (data) => {
-    process.stdout.write(`Your name is: ${data.toString()}`);
-    process.exit();
-  });
-  process.on('exit', () => {
-    process.stdout.write('This important software is now closing\n');
-  });
-}
+const text = "Welcome to Holberton School, what is your name?\n"
+process.stdout.write(`${text}`)
+process.stdin.on("readable", ()=>{
+    const chunk = process.stdin.read();
+    if (chunk !== null){
+        process.stdout.write(`Your name is: ${chunk}`);
+    }
+});
+process.stdin.on("end", ()=>{
+    process.stdout.write("This important software is now closing\n");
+});
